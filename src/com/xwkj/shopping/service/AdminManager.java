679304a6930020dfcc8e34c3b809efbb03ccd4c0
lm2343635 <lm2343635@126.com>
2015-10-30 15:46:28 +0900

    管理员登陆

---
+++ src/com/xwkj/shopping/service/AdminManager.java
@@ -0,0 +1,23 @@
+package com.xwkj.shopping.service;
+
+import javax.servlet.http.HttpSession;
+
+public interface AdminManager 
+{
+	public static final String ADMIN_FLAG="4028e083504139180150414868df000d";
+	
+	/**
+	 * 管理员登陆
+	 * @param username
+	 * @param password
+	 * @return
+	 */
+	boolean login(String username,String password, HttpSession session);
+	
+	/**
+	 * 
+	 * @param session
+	 * @return
+	 */
+	String checkSession(HttpSession session);
+}
